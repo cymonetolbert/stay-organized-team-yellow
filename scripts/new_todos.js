b65208c05044b650297ce0f41a56772423a120ac
elorablank <elorablank@gmail.com>
2023-12-07 14:02:17 -0500

    added html elements, window.onload, and event listeners. getInputs function still WIP

--- scripts/new_todos.js
+++ scripts/new_todos.js
@@ -1,3 +1,34 @@
 "use strict"
 
-console.log("Hey hey!")
+window.onload = function () {
+    const categorySelect = document.querySelector("#category")
+    getCategory(categorySelect)
+
+    const form = document.querySelector("#form")
+    form.onsubmit = getInputs
+}
+
+
+function getCategory(element) {
+    fetch(`http://127.0.0.1:8083/api/categories`)
+    .then(repsone => repsone.json())
+    .then(categories => populateCategories(categories, element))
+}
+function populateCategories(categories, element) {
+    let html = `<option value="none">Please choose a category!</option>`
+    for (const category of categories) {
+        html += `<option value="${category.name}">${category.name}</option>`
+    }
+    element.innerHTML = html
+}
+
+function getInputs(event) {
+    event.preventDefault()
+    let typedUsername = event.target.elements.username.value
+    let selectedCategory = event.target.elements.category.value
+    let typedDescription = event.target.elements.description.value
+    let chosenDate = event.target.elements.date.value
+    console.dir(typedDescription, typedUsername, selectedCategory, chosenDate)
+
+    //not working atm need to come back and fix
+}
