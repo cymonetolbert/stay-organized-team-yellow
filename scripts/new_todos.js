68193fb451fc70acdc0efcf69ede1a64230655e5
elorablank <elorablank@gmail.com>
2023-12-08 15:03:51 -0500

    finished populate functions and get value functions

--- scripts/new_todos.js
+++ scripts/new_todos.js
@@ -4,16 +4,37 @@ window.onload = function () {
     const categorySelect = document.querySelector("#category")
     getCategory(categorySelect)
 
+    const usernameSelect = document.querySelector("#username")
+    getUsername(usernameSelect)
+
     const form = document.querySelector("#form")
     form.onsubmit = getInputs
+
+    const prioritySelect = document.querySelector("#priority")
+    populatePriority(prioritySelect)
+
 }
 
+function getUsername(element) {
+    fetch(`http://127.0.0.1:8083/api/users`)
+    .then(repsone => repsone.json())
+    .then(users => populateUsername(users, element))
+}
+
+function populateUsername(users, element) {
+    let html = `<option value="none">Please choose a username!</option>`
+    for (const user of users) {
+        html += `<option value="${user.id}">${user.username}</option>`
+    }
+    element.innerHTML = html
+}
 
 function getCategory(element) {
     fetch(`http://127.0.0.1:8083/api/categories`)
     .then(repsone => repsone.json())
     .then(categories => populateCategories(categories, element))
 }
+
 function populateCategories(categories, element) {
     let html = `<option value="none">Please choose a category!</option>`
     for (const category of categories) {
@@ -22,13 +43,32 @@ function populateCategories(categories, element) {
     element.innerHTML = html
 }
 
+
+function populatePriority (priority, element) {
+    let html = `<option value="none">Please choose priority level!</option>`
+    for (const level of priority) {
+        html += `<option value="${level.name}">${level.name}</option>`
+    }
+    element.innerHTML = html
+}
+
+
 function getInputs(event) {
     event.preventDefault()
-    let typedUsername = event.target.elements.username.value
-    let selectedCategory = event.target.elements.category.value
-    let typedDescription = event.target.elements.description.value
-    let chosenDate = event.target.elements.date.value
-    console.dir(typedDescription, typedUsername, selectedCategory, chosenDate)
+    let values = []
+    let formElements = event.target.elements
+    console.log(formElements)
 
-    //not working atm need to come back and fix
+    let typedUsername = formElements.username.value
+    let selectedCategory = formElements.category.value
+    let typedDescription = formElements.description.value
+    let chosenDate = formElements.date.value
+    console.log(typedDescription, typedUsername, selectedCategory, chosenDate)
+
+    const newTodoJSON = JSON.stringify({
+        username: typedUsername,
+        category: selectedCategory,
+        completed: false,
+    })
+    console.log(values)
 }
